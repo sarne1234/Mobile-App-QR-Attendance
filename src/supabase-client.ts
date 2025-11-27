905b878b15ed5b93a7e11cb3a8a4ccad0795ff8f
unknown <COMPLAB4@LAB4-30.ASIANCOLLEGE.LOCAL>
2025-11-27 10:06:34 +0800

    first commit

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,8 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 
-const supabaseUrl = 'https://xcmbsbanwhmpkebzirtf.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjbWJzYmFud2htcGtlYnppcnRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyODU3NzUsImV4cCI6MjA3Njg2MTc3NX0.ouJY48DDqk3IrpZV0CvbdPrf3yyXoMSz8u8kKsRuQAE'
+const supabase_Project_Url = import.meta.env.VITE_SUPABASE_URL;
+const supabase_Anon_Key = import.meta.env.VITE_SUPABASE_KEY;
 
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabase_Project_Url, supabase_Anon_Key);
+
+
